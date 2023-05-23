b31b84ca2589ca3978f3ab72d6ba554f5f2702be
gahwa17 <s24527109@gmail.com>
2023-05-24 02:55:47 +0800

    Add Jquery

--- all.js
+++ all.js
@@ -1,8 +1,17 @@
-$(document).ready(function() {
-    $("#navbar-toggler").click(function() {
-        $(this).toggleClass("active");
-        var icon = $(this).find(".navbar-toggler-icon");
-        var currentIcon = icon.text();
-        icon.text(currentIcon === "menu" ? "close" : "menu");
-    });
+// $(document).ready(function() {
+//     $(".navbar-toggler-icon").click(function() {
+//         $(this).toggleClass("active");
+//         var icon = $(this).find(".navbar-toggler-icon");
+//         var currentIcon = icon.text();
+//         icon.text(currentIcon === "menu" ? "close" : "menu");
+//     });
+// });
+
+var navbarToggler = document.querySelector(".navbar-toggler");
+var navbarTogglerIcon = navbarToggler.querySelector(".navbar-toggler-icon");
+
+navbarToggler.addEventListener("click", function() {
+  navbarTogglerIcon.classList.toggle("active");
+  var currentIcon = navbarTogglerIcon.innerHTML;
+  navbarTogglerIcon.innerHTML = (currentIcon === "menu") ? "close" : "menu";
 });
