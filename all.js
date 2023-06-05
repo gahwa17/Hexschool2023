8c84ad51c4e42e80a5f7c6f46f4389f1d6f8954c
gahwa17 <s24527109@gmail.com>
2023-06-06 02:54:36 +0800

    [Swiper] Fix init problem

--- all.js
+++ all.js
@@ -1,17 +1,36 @@
-// $(document).ready(function() {
-//     $(".navbar-toggler-icon").click(function() {
-//         $(this).toggleClass("active");
-//         var icon = $(this).find(".navbar-toggler-icon");
-//         var currentIcon = icon.text();
-//         icon.text(currentIcon === "menu" ? "close" : "menu");
-//     });
-// });
+/* Swiper */
+const swiper = new Swiper('.swiper', {
+  // Default parameter
+  slidesPerView: 3,  // 顯示3個slide元素
+  spaceBetween: 24,  // 間距24px
+  slidesPerGroup: 3, // 3個一組切換
+  // 分頁   
+  pagination: {
+     el: '.swiper-pagination',
+     clickable: true,
+  },
+  // 左右箭頭    
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // 滾動條
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+});
+/*
+Swiper初始化必須放在最前面，否則當頁面中有swiper類元素尚未被swiper初始化，因此無法找到swiper元素
+導致後續的`document.querySelector`返回null，導致整個JS無法順利執行
+*/
+
 
 var navbarToggler = document.querySelector(".navbar-toggler");
 var navbarTogglerIcon = navbarToggler.querySelector(".navbar-toggler-icon");
 
 navbarToggler.addEventListener("click", function() {
-  navbarTogglerIcon.classList.toggle("active");
-  var currentIcon = navbarTogglerIcon.innerHTML;
-  navbarTogglerIcon.innerHTML = (currentIcon === "menu") ? "close" : "menu";
+navbarTogglerIcon.classList.toggle("active");
+var currentIcon = navbarTogglerIcon.innerHTML;
+navbarTogglerIcon.innerHTML = (currentIcon === "menu") ? "close" : "menu";
 });
