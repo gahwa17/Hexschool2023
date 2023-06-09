a319c75f7959996307fe9ff8e99b54b9afa4b78b
gahwa17 <s24527109@gmail.com>
2023-06-10 02:16:53 +0800

    [ Boostrap ] Fix accordion-button border

--- all.js
+++ all.js
@@ -45,12 +45,7 @@ var currentIcon = navbarTogglerIcon.innerHTML;
 navbarTogglerIcon.innerHTML = (currentIcon === "menu") ? "close" : "menu";
 });
 
-$('.question-answer').hide();
-$('.remove-icon').hide();
-$(".question-item").click(function (e) {
-  $(this).toggleClass('active');
-  $(this).find('.add-icon').toggleClass('d-none');
-  $(this).find('.remove-icon').toggleClass('d-block');
-  $(this).find('.collapse-content p').toggleClass('d-block');
-  $(this).find('p').slideToggle(200);
+
+$(".accordion-item").click(function (e) {
+  $(this).toggleClass('show');
 });
